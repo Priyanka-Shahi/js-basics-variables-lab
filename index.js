2bc4555291903f1a7ed2b4ef504ebf04b2418f67
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-13 15:25:41 +1000

    Variables

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+function mostProfitableNeighborhood () {
+    
+}
+
+let companyCeo = 'Susan Smith';
+
+function companyCeo() {
+
+}
+
